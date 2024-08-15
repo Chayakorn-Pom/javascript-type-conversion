92780009132b4ae28ef3c31b550dbc8dadafbcab
Pom <chayakon500@gmail.com>
2024-08-15 09:35:07 +0700

    BP5-POM-Ex1

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,25 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
 
-accountBalance = accountBalance + depositAmountFromUser;
-
 let accountBalanceMessage = "Your account balance is " + accountBalance;
 
 console.log(accountBalanceMessage);
+
+depositAmountFromUser = Number(depositAmountFromUser)
+typeof depositAmountFromUser
+console.log(typeof depositAmountFromUser)
+
+typeof accountBalance
+console.log(typeof accountBalance)
+
+accountBalance = Number(accountBalance)
+typeof accountBalance
+console.log(typeof accountBalance)
+
+accountBalance = accountBalance + depositAmountFromUser
+console.log(accountBalance)
+
+accountBalanceMessage = 'Your account balance is' + " " + accountBalance;
+console.log(accountBalanceMessage)
+
+
